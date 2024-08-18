ca40e301d1956e89e38268bfbbb36663413cf16d
o-ifeanyi <onuifeanyi95@gmail.com>
2024-08-18 20:53:53 +0100

    update link

--- lib/util/constants.dart
+++ lib/util/constants.dart
@@ -1,6 +1,6 @@
 abstract class Constants {
   static const email = 'skriblaapp@gmail.com';
-  static const web = "https://app.skribla.com";
+  static const web = "https://skribla.com";
   static const android =
       "https://play.google.com/store/apps/details?id=com.skribla.android.prod";
   static const iOS = "https://apps.apple.com/us/app/skribla/id6608960206";
