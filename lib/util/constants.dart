faa2c21dd4305a3c003ccf4ce4ef673cfe1d2c3c
o-ifeanyi <onuifeanyi95@gmail.com>
2024-09-03 23:11:34 +0100

    update links

--- lib/util/constants.dart
+++ lib/util/constants.dart
@@ -3,8 +3,8 @@ abstract class Constants {
   static const web = "https://app.skribla.com";
   static const android =
       "https://play.google.com/store/apps/details?id=com.skribla.android.prod";
-  static const iOS = "https://apps.apple.com/us/app/skribla/id6608960206";
-  static const macOS = "https://apps.apple.com/us/app/skribla/id6642646109";
+  static const iOS = "https://apps.apple.com/app/skribla/id6608960206";
+  static const macOS = "https://apps.apple.com/app/skribla/id6642646109";
   static const windows = "";
   static const github = "https://github.com/o-ifeanyi/skribla";
 }
